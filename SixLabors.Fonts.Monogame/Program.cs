8365aada0369b4ae77f43c50d1ef2cdcf4643d33
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2022-07-04 15:50:56 +0300

    Initial commit

---
+++ SixLabors.Fonts.Monogame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SixLabors.Fonts.Monogame.Testing
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1()) game.Run();
+        }
+    }
+}
